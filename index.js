48ebf17b82cb0ba51cc84322b43a4a64d18055e4
harish-nagubadi <Harish.N@veots.com>
2024-10-14 10:08:27 +0530

    first commit2

--- index.js
+++ index.js
@@ -5,21 +5,17 @@ const dotenv = require("dotenv");
 const app = express();
 dotenv.config();
 
-// Allow CORS from localhost:3000
 app.use(cors({
   origin: 'https://finance-frontend-pi.vercel.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
 }));
 
-// Parse incoming request bodies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Connect to your database
 require('./db');
 
-// Register your routes
 const AuthRoutes = require('./Routes/AuthRoutes'); 
 const OtpRouter = require('./Routes/OtpRoutes');
 const AddExpenses = require('./Routes/AddexpensesRoutes');
@@ -32,10 +28,10 @@ app.use("/api", (req, res) => {
   res.send("Hello World!");
 });
 
-// Set the port
+
 const port = process.env.PORT || 3000;
 
-// Start the server
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
